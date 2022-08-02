666e62321a61d428a6576740e3631c651738f767
AcessControlBosch <acesscontrol22@gmail.com>
2022-08-02 10:43:51 -0300

    login_ok

---
+++ acesscontrolfront/pages/screen_cvSeguranca/index.vue
@@ -0,0 +1,124 @@
+<template>
+
+    <div class="container">
+    
+        <Header />
+
+        <div class="content">
+
+            <div class="row-superior">
+
+                <TitlePage />
+
+            </div> 
+
+            <div class="row-inferior">
+
+                <div class="card-info">
+                
+                    <p class="card-title">Segurança</p>
+                    <p class="card-subject">Requisitos de Segurança de acordo com o equipamento:​</p>
+
+
+                </div> 
+
+                <div class="formulario">
+                    
+                    <div class="line-question">
+
+                        <input type="checkbox" class="checkbox" id="um"/>
+                        <label class="p-question" for="um">1 - Proteções</label>
+                        
+
+                    </div>
+
+                    <!--
+                    
+                    <div class="line-question" v-for:"question in allQuestions">
+
+                        <input type="checkbox" class="checkbox" id="um" v-model:"responseQuestions[question.id-1]"/>
+                        <label class="p-question" for="um">{{question.id}} - {{question.name}}</label>
+                        
+                    </div>
+
+                    -->
+
+                </div>
+
+                <div class="align-items-center">
+            
+                    <button class="btn btn-sucess" v-on:click="">Continuar</button>
+                    <button class="btn btn-alert" v-on:click="$router.push('/screen_home')">Cancelar</button>
+            
+                </div>
+            
+            </div>
+
+        </div>
+    
+    </div>
+
+</template>
+
+<script>
+
+export default {
+
+    name: 'screen_cvSeguranca',
+
+    data(){
+
+        return{
+
+            allQuestions: [],
+            responseQuestions: [],
+
+        }
+
+    },
+
+    created(){
+        console.log('this.$store.state.machine', this.$store.state.machine)
+        // this.$axios.get(this.$store.state.BASE_URL + '/greenbook?=' + this.$store.state.machine).then((response) => {
+        //     console.log('oi created')
+        //     this.allQuestions = response.data;
+
+        //     let i = 0;
+
+        //     for(i; i < this.allQuestions.length; i++){
+
+        //         this.responseQuestions[i] = false
+
+        //     }
+
+        // }).catch((error) => {
+
+        //     console.log(error)
+
+        // })
+
+    },
+
+
+    methods: {
+
+        verifyQuestions: function(){
+
+            
+
+
+        }
+
+
+    },
+
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+    @import "@/layouts/_normal_pages/Screen_CvSeguranca.scss";
+    @import "@/layouts/_responsividade/responsividade_grid.scss";
+
+</style>
---
+++ acesscontrolfront/pages/screen_qrCode/index.vue
@@ -0,0 +1,99 @@
+<template>
+    <div class="container">
+    
+        <Header />
+
+        <div class="content">
+
+            <div class="row-superior">
+
+                <TitlePage />
+
+            </div>
+
+            <div class="row-inferior">
+                <!-- <p>{{ error }}</p>
+                <p>{{ decodedString }}</p>
+                <button @click="torch=!torch">TURN ON/ OFF  FLASHLIGHT</button> -->
+                <div class="content-row-inferior">
+                  <p class="subtitle">Escaneie aqui o QR Code da máquina</p>
+                  <div class="qrCodeCamera">
+                    <qrcode-stream @init="onInit" @decode="onDecode" :torch="torch"> </qrcode-stream>
+                  </div>
+                </div>
+                
+                <p class="errorP" style="color:red;">{{ error }}  </p>
+               
+
+            
+            </div>
+
+        </div>
+    
+    </div>
+    
+</template>
+
+<script>
+import { QrcodeStream } from 'vue-qrcode-reader'
+export default {
+  data(){
+    return{
+      error:'',
+      decodedString:'',
+      torch: true,
+      valueQrCode:[]
+    }
+  },
+  components:{
+    QrcodeStream
+  },
+  methods: {
+    async onInit( promise ){
+      try {
+      
+      const { capabilities } = await promise
+      console.log('-----QR CODE -----')
+      console.log(this.decodedString)
+      // successfully initialized
+    } catch (error) {
+      if (error.name === 'NotAllowedError') {
+        this.error = "user denied camera access permisson"
+      } else if (error.name === 'NotFoundError') {
+        this.error = "no suitable camera device installed"
+      } else if (error.name === 'NotSupportedError') {
+        this.error = "page is not served over HTTPS (or localhost)"
+      } else if (error.name === 'NotReadableError') {
+        this.error = "maybe camera is already in use"
+      } else if (error.name === 'OverconstrainedError') {
+        this.error = "did you requested the front camera although there is none?"
+      } else if (error.name === 'StreamApiNotSupportedError') {
+        this.error = "browser seems to be lacking features"
+      }
+    } finally {
+      // hide loading indicator
+    }
+  
+    },
+    onDecode(decodedString){
+      console.log('-----QR CODE onDecode-----');
+      console.log('onDecode', decodedString);
+      this.valueQrCode.push({
+        value: decodedString
+      })
+      console.log(this.valueQrCode.value)
+      this.$store.dispatch("SET_MACHINE",this.valueQrCode);
+      console.log('this.$store.state.machine',this.$store.state.machine)
+      // $router.push('/screen_home')
+      // this.decodedString = decodedString;
+      window.location.replace('/screen_home');
+    }
+}
+}
+</script> 
+
+
+<style lang="scss" scoped>
+   @import "@/layouts/_normal_pages/Screen_qrCode.scss";
+   @import "@/layouts/_responsividade/responsividade_formularios.scss";
+</style>
